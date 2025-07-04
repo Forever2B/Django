c33b1de6cc62d09ed5ef1da9c4836ec7abfb5319
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-07-04 14:52:37 -0300

    commit temporário de correções

--- EBAC_Blog/tests/models/test_post.py
+++ EBAC_Blog/tests/models/test_post.py
@@ -16,6 +16,7 @@ class UserFactory(factory.django.DjangoModelFactory):
     email = factory.Faker("safe_email")
     username = factory.LazyAttribute(lambda x: faker.name())
 
+
     @classmethod
     def _prepare(cls, create, **kwargs):
         password = kwargs.pop("password", None)
@@ -24,14 +25,14 @@ class UserFactory(factory.django.DjangoModelFactory):
             user.set_password(password)
             if create:
                 user.save()
-        return user
-
-
+            return user
+        
 class PostFactory(factory.django.DjangoModelFactory):
     title = factory.LazyAttribute(lambda x: faker.sentence())
     created_on = factory.LazyAttribute(lambda x: now())
     author = factory.SubFactory(UserFactory)
     status = 0
+
 
     class Meta:
         model = Post
